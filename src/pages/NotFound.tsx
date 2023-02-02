4ac27313df2d7fe6dc58384f07f30ccc014029a4
daofengcode <daofengcode@protonmail.com>
2023-02-02 14:31:52 +0800

    重构404页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Button, Result } from 'antd';
+import { useNavigate } from "react-router-dom";
+
 
 type Props = {}
 
 const NotFound = (props: Props) => {
+  const navigate = useNavigate()
+  const onClick = () => {
+    navigate("/")
+  }
   return (
-    <div>Page not found.</div>
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在。"
+      extra={<Button type="primary" onClick={onClick}>返回首页</Button>}
+    />
   )
 }
 
